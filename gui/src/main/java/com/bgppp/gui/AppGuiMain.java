fdc44eae4eb99817da694c18465a2bddcaf27704
Mikle <mikle.blv@gmail.com>
2014-02-11 15:50:31 +0200

    New GUI

--- gui/src/main/java/com/bgppp/gui/AppGuiMain.java
+++ gui/src/main/java/com/bgppp/gui/AppGuiMain.java
@@ -1,32 +1,44 @@
 package com.bgppp.gui;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JSplitPane;
+import javax.swing.ImageIcon;
 import javax.swing.JTextPane;
 import javax.swing.JTextField;
-import javax.swing.JTextArea;
 import javax.swing.JLabel;
+
+import java.awt.Graphics;
 import java.awt.Font;
+
 import javax.swing.SwingConstants;
-import java.awt.Component;
-import javax.swing.Box;
-import java.awt.Dimension;
+
 import java.awt.Color;
-import javax.swing.border.LineBorder;
-import javax.swing.JList;
-import javax.swing.ListSelectionModel;
-import javax.swing.border.BevelBorder;
-import java.awt.Choice;
-import javax.swing.AbstractListModel;
+
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JFormattedTextField;
-import java.awt.Canvas;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+
+
+
+
+
+//import com.bgppp.gui.ObjectMapper;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import javax.swing.Icon;
 
 
 public class AppGuiMain extends JFrame {
@@ -45,7 +57,13 @@ public class AppGuiMain extends JFrame {
 	private final JLabel lblIpAddress = new JLabel("IP address");
 	private final JTextField textField = new JTextField();
 	private final JLabel lblSpeed = new JLabel("Speed");
-	private final Canvas canvas = new Canvas();
+	private final JButton btnGo = new JButton("Go!");
+	public static ImageIcon image = new ImageIcon("C:\\MIKLE MY DATA\\Misc\\Eclipse\\workspace\\BGP\\bgppp\\router-image.png");
+	private final static JLabel lblNewLabel = new JLabel((Icon) null);
+	private final static JLabel label = new JLabel((Icon) null);
+	private final static JLabel label_1 = new JLabel((Icon) null);
+	private final static JLabel label_2 = new JLabel((Icon) null);
+	private final static JLabel label_3 = new JLabel((Icon) null);
 
 	/**
 	 * Launch the application.
@@ -125,10 +143,91 @@ public class AppGuiMain extends JFrame {
 		lblSpeed.setBounds(300, 103, 46, 14);
 		
 		contentPane.add(lblSpeed);
-		canvas.setBackground(Color.GRAY);
-		canvas.setForeground(Color.WHITE);
-		canvas.setBounds(10, 17, 249, 233);
 		
-		contentPane.add(canvas);
+		btnGo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				 String textIp = formattedTextField.getText();
+				 String textSpeed = textField.getText();
+				 String textComboBox = comboBox.getSelectedItem().toString();
+				txtpnSomeDataFrom.setText(textComboBox + "  " + textIp + "  " + textSpeed);
+				transfer (textComboBox, textIp, textSpeed);
+				paintRouter (textComboBox);
+			}
+		});
+		btnGo.setBounds(265, 128, 59, 23);
+		
+		contentPane.add(btnGo);
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setBackground(Color.WHITE);
+		lblNewLabel.setBounds(10, 16, 66, 49);
+				
+		contentPane.add(lblNewLabel);
+		label.setForeground(Color.WHITE);
+		label.setBackground(Color.WHITE);
+		label.setBounds(32, 89, 66, 49);
+		
+		contentPane.add(label);
+		label_1.setForeground(Color.WHITE);
+		label_1.setBackground(Color.WHITE);
+		label_1.setBounds(32, 162, 66, 49);
+		
+		contentPane.add(label_1);
+		label_2.setForeground(Color.WHITE);
+		label_2.setBackground(Color.WHITE);
+		label_2.setBounds(142, 140, 66, 49);
+		
+		contentPane.add(label_2);
+		label_3.setForeground(Color.WHITE);
+		label_3.setBackground(Color.WHITE);
+		label_3.setBounds(153, 43, 66, 49);
+		
+		contentPane.add(label_3);
+	}
+	
+	public static void transfer(String textComboBox, String textIp, String textSpeed) {
+				
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, String> map = new HashMap<String, String>();
+		
+		map.put("Number", textComboBox);
+		map.put("IP", textIp);
+		map.put("Speed", textSpeed);
+		
+		File foo = new File("C:\\MIKLE MY DATA\\example.json");
+		
+		foo.setExecutable(true);
+	    foo.setReadable(true);
+	    foo.setWritable(true);
+		
+		try {
+			mapper.writeValue(foo, map);		
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 			
+	}
+	
+	
+	@SuppressWarnings("null")
+	public static void paintRouter (String textComboBox) {
+		
+		lblNewLabel.setIcon(image);
+		label.setIcon(image);
+		label_1.setIcon(image);
+		label_2.setIcon(image);
+		label_3.setIcon(image);
+		
+		Graphics g = null;
+		g.drawLine(43, 65, 65, 138);
+			
+		/*AppGuiMain frame = new AppGuiMain();
+	    frame.setSize(300, 450);
+	    frame.setTitle("Picture Test");
+	    frame.setLocationRelativeTo(null);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setVisible(true);*/		
+
+		
 	}
 }
+
